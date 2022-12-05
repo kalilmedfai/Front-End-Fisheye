ab0958a20181cbce3f5ad5ca51a1ce1689d71060
kalilmedfai <kalilmedfai@hotmail.fr>
2022-12-05 13:54:39 +0100

    test 1

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,9 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        fetch('data/photographers.json')
+            .then(r => console.log(r))
+
         let photographers = [
             {
                 "name": "Ma data test",
