98029ce701552f33a2b1507fa09e26809fe48155
kalilmedfai <kalilmedfai@hotmail.fr>
2022-12-19 16:26:30 +0100

    Récupération id du photographe depuis l'url pour la page photographer.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const a = document.createElement( 'a' );
+        a.setAttribute("href", `photographer.html?id=${id}`)
+        a.setAttribute("id", "photographer__link")
+        // ajout de la balise article que l'on nommme article
         const article = document.createElement( 'article' );
+        // ajout de la balise img que l'on nommme img
         const img = document.createElement( 'img' );
+        // ajout de l'attribut src
         img.setAttribute("src", picture)
+        // ajout de la balise h2 que l'on nommme h2
         const h2 = document.createElement( 'h2' );
+        // affichage de h2
         h2.textContent = name;
         // ajout de la balise p que l'on nommme p
         const p = document.createElement ( 'p' );
+        // changement de la couleur p
+        p.style.color = "#901C1C"
         // affichage de p
         p.textContent = city + ", " + country;
         // ajout de la balise p que l'on nommme p1
@@ -20,12 +30,13 @@ function photographerFactory(data) {
         // ajout de la balise p que l'on nommme p2
         const p2 = document.createElement ( 'p' );
         // affichage de p2
-        p2.textContent = price + "/jour";
-        p2.style.color =
+        p2.textContent = price + "€/jour";
+        p2.style.color = "#757575"
+        article.appendChild(a);
         // ajout visuel img
-        article.appendChild(img);
+        a.appendChild(img);
         // ajout visuel h2
-        article.appendChild(h2);
+        a.appendChild(h2);
         // ajout visuel p
         article.appendChild(p);
         // ajout visuel p1
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+
+
+fetch('data/photographers.json')
+    .then(res => res.json())
+    //.then(res2 => console.log(res2))
+    .then(res => {
+        let params = new URLSearchParams(location.search)
+
+        let id_photographer = parseInt(params.get("id"), 10)
+
+        console.log(res)
+        console.log(id_photographer)
+        let photographer_find = res.photographers.find(
+            function (photographer) {
+                console.log(id_photographer, photographer.id, id_photographer === photographer.id)
+                if(id_photographer === photographer.id){
+                    return true
+                } else {
+                    return false
+                }
+            }
+        )
+        console.log(photographer_find)
+    })
